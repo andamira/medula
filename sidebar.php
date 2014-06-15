a4392681e33ab73b02d5179636a1b61c3079dfd8
joseluis <senonvero@gmail.com>
2014-06-15 19:11:35 +0200

    updated sidebar template

--- sidebar.php
+++ sidebar.php
@@ -6,14 +6,8 @@
 
 	<?php else : ?>
 
-		<?php
-			/*
-			 * This content shows up if there are no widgets defined in the backend.
-			*/
-		?>
-
 		<div class="no-widgets">
-			<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'osea-theme' ); ?></p>
+			<p><?php _e( 'Empty widget area', 'osea-theme' ); ?></p>
 		</div>
 
 	<?php endif; ?>
