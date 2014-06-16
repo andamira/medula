6ed9c410320e383518afcb6bdbbe5709437d8e13
joseluis <senonvero@gmail.com>
2014-06-16 14:11:08 +0200

    added schema functions template

---
+++ lib/schema.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Schema template
+ * ************************************************************
+ *
+ * Functions for outputting schema.org html tags
+ * 
+ */
+
+
+function html_tag_schema()
+{
+	$schema = 'http://schema.org/';
+
+	// Is single post
+	if( is_single() ) {
+		$type = "Article";
+	}
+	// Contact form page ID
+	else if( is_page(1) )  	{
+		$type = 'ContactPage';
+	}
+	// Is author page
+	elseif( is_author() ) {
+		$type = 'ProfilePage';
+	}
+	// Is search results page
+	elseif( is_search() ) {
+		$type = 'SearchResultsPage';
+	}
+	// Is of movie post type
+	elseif(is_singular('movies')) {
+		$type = 'Movie';
+	}
+	// Is of book post type
+	elseif(is_singular('books')) {
+		$type = 'Book';
+	}
+	else {
+		$type = 'WebPage';
+	}
+
+	echo 'itemscope="itemscope" itemtype="' . $schema . $type . '"';
+}
+
+
+
+
+
+
+
+?>
