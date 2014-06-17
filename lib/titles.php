dc8bfe6ac53a1dcaa6133e0329716eed0e6ea555
joseluis <senonvero@gmail.com>
2014-06-17 17:12:46 +0200

    fixed title with link

--- lib/titles.php
+++ lib/titles.php
@@ -24,14 +24,14 @@ function osea_entry_title( $htag, $with_link = false, $headline = false ) {
 	$title  = "<$htag ". 'class="entry-title"';
 
 	if ( $with_link ) {
-		$title .= '><a href="' . get_the_permalink() . '" rel="bookmark" title="' . the_title_attribute('', '', false) . '">' . get_the_title() . '</a>';
+		$title .= '><a href="' . get_the_permalink() . '" rel="bookmark" title="' . the_title_attribute( 'echo=0' ) . '">' . get_the_title() . '</a>';
 	} else {
 		// NOTE: if no link, assumes it is headline
 		$title .= ' itemprop="headline">' . get_the_title();
 	}
 
 	// Shows Edit Post Link ( defined in /lib/edit-post.php )
-	$title .= osea_entry_edit_post( false ); // echo = false
+	$title .= osea_entry_edit_post( false );
 
 	$title .= "</$htag>";
 
