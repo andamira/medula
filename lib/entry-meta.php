b90ebc5c5fc2dd79543fb65c8f6be7e7321d9935
joseluis <senonvero@gmail.com>
2014-06-17 12:47:32 +0200

    updated comments & minor changes

--- lib/entry-meta.php
+++ lib/entry-meta.php
@@ -2,15 +2,14 @@
 /*
  * Functions outputting ENTRY META fields
  *
- * INDEX:
- * 		DateTime
- * 		Author
+ * 		1 DateTime
+ * 		2 Author
  *
- * 		Byline
- * 		Categories + custom
- * 		Tags + custom
+ * 		3 Byline
+ * 		4 Categories + custom
+ * 		5 Tags + custom
  *
- * 		Entry Edit Post
+ * 		6 Entry Edit Post
  *
  * Feel free to modify them in any way you like. For example:
  * You could add a parameter to the author function to choose
@@ -20,7 +19,15 @@
 
 
 /*
- * Returns the entry author
+ * 1 Returns the entry datetime
+ */
+function osea_get_entry_meta_date() {
+	return( '<time class="entry-date updated" datetime="' . get_the_time('Y-m-dTH:i:sO') . '" itemprop="datePublished" pubdate>' . get_the_time(get_option('date_format')) . '</time>' );
+}
+
+
+/*
+ * 2 Returns the entry author
  *
  * Codex:
  * http://codex.wordpress.org/Function_Reference/get_the_author_meta
@@ -48,23 +55,13 @@ function osea_get_entry_meta_author() {
 	return $author;
 }
 
-/*
- * Returns the entry datetime
- */
-function osea_get_entry_meta_date() {
-	return( '<time class="entry-date updated" datetime="' . get_the_time('Y-m-dTH:i:sO') . '" itemprop="datePublished" pubdate>' . get_the_time(get_option('date_format')) . '</time>' );
-}
-
 
 /*
- * Prints the byline
+ * 3 Prints the byline
  */
 function osea_entry_meta_byline() {
-
 	$byline  = '<span class="entry-byline byline">';
-
 	$byline .= sprintf(__( 'Posted %1$s by %2$s', 'osea-theme' ), osea_get_entry_meta_date(), osea_get_entry_meta_author( 'index' ) );
-
 	$byline .= '</span>';
 
 	echo $byline;
@@ -73,7 +70,7 @@ function osea_entry_meta_byline() {
 
 
 /*
- * Prints the categories
+ * 4 Prints the categories
  */
 function osea_entry_meta_categories( $custom = false ) {
 
@@ -100,16 +97,12 @@ function osea_entry_meta_categories( $custom = false ) {
 
 
 /*
- * Prints the tags
+ * 5 Prints the tags
  */
 function osea_entry_meta_tags( $custom = false) {
-
 	$tags_title = '<span class="entry-tags-title">' . __( 'Tags: ', 'osea-theme' ) . '</span>';
-
 	$tags  = '<span class="entry-tags">';
-
 	$tags .= '<span class="tags">' . get_the_tag_list( $tags_title, ', ' ) . '</span>';
-
 	$tags .= '</span>';
 
 	echo $tags;
@@ -118,17 +111,11 @@ function osea_entry_meta_tags( $custom = false) {
 
 
 /*
- * Prints the Edit Post Link
+ * 6 Prints the Edit Post Link
  */
-function osea_entry_edit_post( $edit_text = null ) {
+function osea_entry_edit_post() {
 	$edit = '<span class="entry-edit-link"><a href="' . get_edit_post_link() . '">';
-
-	// if no text is provided use an icon
-	if ( ! $edit_text ) {
-		$edit .= '<i class="dashicons dashicons-edit"></i>';
-	}
-	$edit .= __( $edit_text, 'osea-theme' );
-
+	$edit .= '<i class="dashicons dashicons-edit"></i>';
 	$edit .= '</a></span>';
 
 	echo $edit;
@@ -136,6 +123,3 @@ function osea_entry_edit_post( $edit_text = null ) {
 
 
 
-
-
-?>
