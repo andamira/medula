5de99fd4f63138ec3713ad90bd96d976f7d7e627
joseluis <senonvero@gmail.com>
2014-06-19 23:44:52 +0200

    added searchform

---
+++ searchform.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+
+<!-- Search Form Type 1: Default -->
+
+<li id="search">
+    <label for="s"><?php _e('Search:', 'osea-theme')?></label>   
+    <form id="searchform" method="get" action="/index.php">
+        <div>
+            <input type="text" name="s" id="s" size="15" /><br />
+            <input type="submit" value="<?php _e('Search','osea-theme')?>" />
+        </div>
+    </form>
+</li>
+
+
+<!-- Search Form Type 2: simple clean + icon -->
+
+<form id="searchform" method="get" action="/index.php">
+    <input type="text" name="s" id="s" />
+    <button type="submit" title="<?php _e('Search','osea-theme')?>">
+        <i class="dashicons-search dashicons"></i>
+    </button>
+</form>
+
+
+<!-- Search Form Type 2: simple clean + icon -->
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<input type="text" value="" name="s" id="s" placeholder="<?php _e('Type & Hit Enter')?>" />
+</form>
+
+
+/**/
+?>
+
+<form id="searchform" method="get" action="/index.php">
+    <input type="text" name="s" id="s" placeholder="<?php _e('Search . . .','osea-theme')?>" />
+    <button type="submit" title="<?php _e('Search','osea-theme')?>">
+        <i class="dashicons-search dashicons"></i>
+    </button>
+</form>
