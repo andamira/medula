afac0aa23c8cd526d8b5c1758d17c29b39eaa5f3
joseluis <senonvero@gmail.com>
2014-06-20 09:59:10 +0200

    added archive-custom_type template

---
+++ archive-custom_type.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * CUSTOM POST TYPE ARCHIVE TEMPLATE
+ *
+ * This is the custom post type archive template. If you edit the custom post type name,
+ * you've got to change the name of this template to reflect that name change.
+ *
+ * For Example, if your custom post type is called "register_post_type( 'bookmarks')",
+ * then your template name should be archive-bookmarks.php
+ *
+ * For more info: http://codex.wordpress.org/Post_Type_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+<main role="main">
+
+	<header class="page-header">
+		<h1 class="page-title"> <?php post_type_archive_title(); ?></h1>
+	</header>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+			<header class="entry-header">
+				<?php osea_entry_title( 'h2', true ); ?>
+                <div class="entry-meta"><?php osea_entry_meta_byline(); osea_entry_meta_tags() ?></div>
+			</header>
+
+			<section class="entry-content">
+				<?php the_excerpt(); ?>
+			</section>
+
+			<footer class="entry-footer">
+				<div class="entry-meta"><?php osea_entry_meta_tags(); osea_entry_meta_categories(); ?></div>
+			</footer>
+
+		</article>
+
+	<?php endwhile; ?>
+
+		<?php osea_page_navi(); ?>
+
+	<?php else : ?>
+
+		<?php osea_post_not_found( basename( __FILE__ ) ); ?>
+
+	<?php endif; ?>
+
+</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
