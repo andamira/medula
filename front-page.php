64b294212bdcfe3c76f3e0ee8e3c75ee4f723f06
joseluis <senonvero@gmail.com>
2014-06-20 19:16:06 +0200

    added front-page template

---
+++ front-page.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<main role="main">
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+			<header class="entry-header">
+				<?php osea_debug_showfile( __FILE__ ); ?>
+				<?php osea_entry_title( 'h1' ); ?>
+			</header>
+
+			<section class="entry-content">
+				<?php the_content(); ?>
+				<?php osea_page_links(); ?>
+			</section>
+
+			<footer class="entry-footer">
+			</footer>
+
+			<?php comments_template( '', true ); ?>
+
+		</article>
+
+	<?php endwhile; else: ?>
+
+		<?php osea_post_not_found( basename( __FILE__ ) ); ?>
+
+	<?php endif; ?>
+
+</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
