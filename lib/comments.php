5a9c8ab94d389c8155b346cc270aa23d35d412af
joseluis <senonvero@gmail.com>
2014-06-21 21:05:28 +0200

    enabled improved gravatar system & improved comments code

--- lib/comments.php
+++ lib/comments.php
@@ -38,33 +38,33 @@ function osea_comments_count() {
  *
  */
 function osea_comments_layout( $comment, $args, $depth ) {
-   $GLOBALS['comment'] = $comment; ?>
-  <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
-	<article>
+	$GLOBALS['comment'] = $comment; ?>
 
-      <header class="comment-author vcard">
-        <?php
-        /*
-		 * Custom gravatar call
-		 *
-		 * this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display
-		 * comment gravatars on larger screens only. What this means is that on larger posts, mobile sites
-		 * don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd
-		 * like to change it back, just replace it with the regular wordpress gravatar call:
-		 * 
-		 * echo get_avatar($comment,$size='32',$default='<path_to_url>' );
-        */
-        ?>
-        <?php
-          // create variable
-          $bgauthemail = get_comment_author_email();
-        ?>
-        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/lib/img/nothing.gif" />
-        <?php // end custom gravatar call ?>
-        <?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'osea-theme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'osea-theme' ),'  ','') ) ?>
-        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'osea-theme' )); ?> </a></time>
+	<div id="comment-<?php comment_ID(); ?>" <?php comment_class(); ?>>
 
-	  </header>
+		<article>
+
+		  <header class="comment-author vcard">
+			<?php
+			/*
+			 * Custom gravatar call
+			 *
+			 * this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display
+			 * comment gravatars on larger screens only. What this means is that on larger posts, mobile sites
+			 * don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd
+			 * like to change it back, just replace it with the regular wordpress gravatar call:
+			 * 
+			 * echo get_avatar($comment,$size='32',$default='<path_to_url>' );
+			*/
+			$bgauthemail = get_comment_author_email();
+			?>
+			<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/lib/img/nothing.gif" />
+
+			<?php //printf(__( '<cite class="fn">%1$s</cite> %2$s', 'osea-theme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'osea-theme' ),'  ','') ) ?>
+			<?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'osea-theme' ), get_comment_author_link(), osea_edit_comment_link() ) ?>
+			<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'osea-theme' )); ?> </a></time>
+
+		</header>
 
       <?php if ($comment->comment_approved == '0') : ?>
         <div class="alert alert-info">
@@ -72,7 +72,7 @@ function osea_comments_layout( $comment, $args, $depth ) {
         </div>
 	  <?php endif; ?>
 
-      <section class="comment_content cf">
+      <section class="comment-content">
         <?php comment_text() ?>
 	  </section>
 
