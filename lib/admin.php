a0b53766e76156c7a36abc538954a63db6043b0e
joseluis <senonvero@gmail.com>
2014-06-22 23:03:37 +0200

    added more default widgets for removal

--- lib/admin.php
+++ lib/admin.php
@@ -26,10 +26,12 @@ function disable_default_dashboard_widgets() {
 	remove_meta_box( 'dashboard_right_now', 'dashboard', 'core' );			// Right Now Widget
 	remove_meta_box( 'dashboard_activity', 'dashboard', 'core' );			// Activity widget
 	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'core' );	// Comments Widget
-	//remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'core' );		// Incoming Links Widget
+	//remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'core' );	// Incoming Links Widget
 	//remove_meta_box( 'dashboard_plugins', 'dashboard', 'core' );			// Plugins Widget
 	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'core' );		// Quick Press Widget
 	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'core' );		// Recent Drafts Widget
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'core' );	// WordPress News
+	remove_meta_box( 'dashboard_secondary', 'dashboard', 'core' );// 
 
 	// Plugins' dashboard boxes
 	remove_meta_box( 'yoast_db_widget', 'dashboard', 'normal' );			// Yoast's SEO
