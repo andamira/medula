f4b12d300e16b6c0bc395bffc075564875bdb738
joseluis <senonvero@gmail.com>
2014-07-05 19:32:51 +0200

    ext libs refactor

--- lib/ext/libs.php
+++ lib/ext/libs.php
@@ -2,61 +2,102 @@
 /**
  * External Libraries Template
  *
+ *		1 GLOBALS
+ *		2 CHOOSE LIBRARIES
+ *			2. Typography
+ *			2. 
+ *			2. Maps
+ *			2.
+ *		3 SETUP LIBRARIES
  */
 
 
-/**
- * ENQUEUE EXTRA LIBRARIES & STYLES
- *
- * @see:source http://stackoverflow.com/a/19263523 
- */
-function extra_libraries_styles() {
+function extra_libraries_styles () {
 
 	/**
-	 * OPTIONS
+	 * 1 GLOBALS
 	 */
+	// Set this to true to use a minified version of the scripts
+	$MINIFY_EXT_LIBS = false;
+	$EXT_DIR = get_stylesheet_directory_uri() . '/lib/ext/libs/';
 
-	// Here choose which libraries to load
-	$load_jquery =		false;  // latest jquery
-	//
-	$load_cycle2 =		false;  // versatile sliders
-	$load_qtip2 =		false;  // advanced bubble tips system
-	$load_dynatable =	false;  // powerful tables
-
-	// Set this to true to usea  minified version of the scripts
-	$_PRODUCTION_ = false;
-
-	if ($_PRODUCTION_) { $MIN = ".min"; } else { $MIN = ""; }
-	$EXT_DIR = get_stylesheet_directory_uri() . '/lib/ext/';
+	if ($MINIFY_EXT_LIBS) { $MIN = ".min"; } else { $MIN = ""; }
 
 
 	/**
-	 * LIBRARIES
+	 * 2 CHOOSE LIBRARIES TO LOAD
+	 *
+	 * Set to true the libraries you want to use
 	 */
 
-	// JQUERY ( Google CDN )
+	// Latest jquery
+	$load_jquery =		false;
+
+	// Sliders
+	$load_cycle2 =		false;
+
+	// Bubble Tips
+	$load_qtip2 =		false;
+
+	// Tables
+	$load_dynatable =	false;
+
+	// Typography TODO
+	$load_lettering =	false;	// letteringjs.com
+
+
+	/**
+	 * 3 SETUP THE LIBRARIES
+	 *
+	 * @see:source http://stackoverflow.com/a/19263523 
+	 */
+
+	/**
+	 * JQUERY
+	 * Google CDN
+	 */
 	if ( $load_jquery ) { 
 		wp_deregister_script( 'jquery' );
 		wp_register_script( 'jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js", false, null );
 		wp_enqueue_script( 'jquery' );
 	}
 
-	// [SLIDER] JQUERY CYCLE2 ( http://jquery.malsup.com/cycle2/ ) ( https://github.com/malsup/cycle2 )
+	/**
+	 * JQUERY CYCLE2
+	 * 
+	 * SLIDER
+	 * @see http://jquery.malsup.com/cycle2/ 
+	 * @see https://github.com/malsup/cycle2
+	 */
 	if ( $load_cycle2 ) { 
 		wp_register_script('cycle2_js', $EXT_DIR .'cycle2/jquery.cycle2'. $MIN .'.js', 'jquery' );
 		wp_enqueue_script( 'cycle2_js');
 	}   
-	// [BUBBLE TIPS] JQUERY QTIP2 ( http://qtip2.com/ ) ( https://github.com/qTip2/qTip2  )
+
+	/**
+	 * JQUERY QTIP2 
+	 *
+	 * BUBBLE TIPS
+	 * @see http://qtip2.com/ 
+	 * @see https://github.com/qTip2/qTip2
+	 */
 	if ( $load_qtip2 ) { 
 		// there are more features available ( http://qtip2.com/download#builder-features )
 		wp_register_script( 'qtip2_js', '//cdnjs.cloudflare.com/ajax/libs/qtip2/2.2.0/basic/jquery.qtip'. $MIN .'.js', 'jquery' );
 		wp_enqueue_script( 'qtip2_js');
 		wp_register_style( 'qtip2_css', '//cdnjs.cloudflare.com/ajax/libs/qtip2/2.2.0/basic/jquery.qtip'. $MIN .'.css' );
 		wp_enqueue_style( 'qtip2_css');
-	}   
-	// [TABLES] DYNATABLE ( https://www.dynatable.com/ ) ( https://github.com/alfajango/jquery-dynatable )
+	}
+
+	/**
+	 * DYNATABLE
+	 *
+	 * TABLES
+	 * @see https://www.dynatable.com/
+	 * @see https://github.com/alfajango/jquery-dynatable
+	 */
 	if ( $load_dynatable ) { 
-		// This is a customized version of Dynatable for adding localization support
+		// This is a customized version of Dynatable that adds localization support
 		wp_register_script('dynatable_js', $EXT_DIR .'dynatable/jquery.dynatable'. $MIN .'.js', 'jquery' ); // used http://jscompress.com/
 		// localization
 		wp_localize_script('dynatable_js', 'dynatable_txt', array(
@@ -74,6 +115,7 @@ function extra_libraries_styles() {
 		));
 		wp_enqueue_script( 'dynatable_js');
 	}
+
 
 }
 add_action( 'wp_enqueue_scripts', 'extra_libraries_styles' );
