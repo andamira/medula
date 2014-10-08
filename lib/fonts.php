4bdbaf0e8a9849af9af63743b64a8a6e0f39cd40
joseLuís <senonvero@gmail.com>
2014-10-08 21:43:30 +0200

    change the styles hook

--- lib/fonts.php
+++ lib/fonts.php
@@ -12,8 +12,7 @@
  */
 
 function osea_fonts() {
-	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700');
-	wp_enqueue_style( 'googleFonts');
+	wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700');
 }
 add_action('wp_print_styles', 'osea_fonts');
 
