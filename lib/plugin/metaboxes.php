0742d2956b533798f8e08a8f8d2eeb207b31586c
joseLuís <senonvero@gmail.com>
2014-12-08 17:26:18 +0100

    Updated metabox plugin recommendation

--- lib/plugin/metaboxes.php
+++ lib/plugin/metaboxes.php
@@ -5,7 +5,7 @@
  *
  * Several frameworks for adding custom metaboxes:
  *
- * https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress
+ * https://github.com/WebDevStudios/CMB2
  * https://github.com/humanmade/Custom-Meta-Boxes
  * https://github.com/rilwis/meta-box
  */
