d281fcc6a288202a3254313b7be0cc269ee64abf
joseLuís <senonvero@gmail.com>
2014-12-14 00:54:55 +0100

    improved comments for using fonts

--- lib/fonts.php
+++ lib/fonts.php
@@ -2,7 +2,21 @@
 /*
  * Fonts template
  *
- * 		1 Google fonts
+ *      1 Google fonts
+ *
+ *
+ * To use dowloaded fonts put them in:
+ * lib/fonts/ & then load them from:
+ * lib/scss/modules/_typography.scss
+ *
+ * You can find free fonts in:
+ * @see http://www.google.com/fonts/
+ * @see http://www.fontsquirrel.com/
+ * @see http://openfontlibrary.org/
+ *
+ * And pay fonts in:
+ * @see https://typekit.com/
+ * @see http://www.fontspring.com/
  */
 
 
@@ -10,11 +24,9 @@
  * 1 GOOGLE FONTS
  * ************************************************************
  */
-
 function osea_fonts() {
 	wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700');
 }
 add_action('wp_enqueue_scripts', 'osea_fonts');
-
 
 
