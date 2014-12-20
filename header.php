97a7b13b4dd9ca628bac09209f6b3395f2049844
joseLuís <senonvero@gmail.com>
2014-12-20 15:11:52 +0100

    removed unnecesary bits

--- header.php
+++ header.php
@@ -35,7 +35,7 @@ if ( defined( 'OSEA_IE8_SUPPORT' ) && OSEA_IE8_SUPPORT ) { ?>
 	<?php // mobile meta ?>
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
