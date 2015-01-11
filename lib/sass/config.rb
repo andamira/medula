0c48b16c0b744dd700878d94c3d17f833a820afa
joseLuís <senonvero@gmail.com>
2015-01-11 18:34:03 +0100

    refactored compass config

--- lib/sass/config.rb
+++ lib/sass/config.rb
@@ -1,28 +1,48 @@
-# Compass is a great cross-platform tool for compiling SASS. 
-# This compass config file will allow you to dive right in.
-# http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
+#
+# This is the compass configuration file
+# Compass is a great cross-platform tool for compiling SASS.
 #
 # http://compass-style.org/help/documentation/configuration-reference/
+# http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
+
+#
 # 0. Dependencies
-require "susy" 
-require "breakpoint" 
-require 'sassy-maps'
-#require 'SassyLists'
+#
+
+require "compass/import-once/activate"
+require "susy"
+require "breakpoint"
+require "sassy-maps"
+#require "SassyLists"
 
 
+#
 # 1 Set this to the root of your project when deployed
-http_path = "/"				
+#
 
+http_path = "/"
+
+
+#
 # 2. You can select your preferred output style here (can be overridden via the command line):
+#
+
 output_style = :expanded
 #output_style = :compressed	# On launch, uncomment this line and comment the above one
 
+
+#
 # 3. To disable debugging comments that display the original location of your selectors. Uncomment:
+#
+
 # line_comments = false
 
 
-# 4. Probably don't need to touch these
+#
+# 4. Default options. You'll probably don't have to touch these
+#
+
 css_dir = "../css"
 sass_dir = "./"
 images_dir = "../img"
@@ -31,6 +51,6 @@ fonts_dir = "../fonts"
 relative_assets = true
 environment = :development
 
-
-# Really don't touch this
+# don't touch this
 preferred_syntax = :scss
+
