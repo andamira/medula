d548a6b20f3023efbcec2d4bde2b03e44d4aaf75
joseLuís <senonvero@gmail.com>
2015-01-11 14:46:28 +0100

    added sassy-maps and SassyLists functions; refactored documentation; added dependency functions documentation

--- lib/sass/config.rb
+++ lib/sass/config.rb
@@ -2,34 +2,35 @@
 # This compass config file will allow you to dive right in.
 # http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 #
-# Information:
-#
-
+# http://compass-style.org/help/documentation/configuration-reference/
 
 # 0. Dependencies
 require "susy" 
 require "breakpoint" 
+require 'sassy-maps'
+#require 'SassyLists'
 
-#########
-# 1. Set this to the root of your project when deployed:
-http_path = "/" 
 
-# 2. probably don't need to touch these
+# 1 Set this to the root of your project when deployed
+http_path = "/"				
+
+# 2. You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+#output_style = :compressed	# On launch, uncomment this line and comment the above one
+
+# 3. To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# 4. Probably don't need to touch these
 css_dir = "../css"
 sass_dir = "./"
 images_dir = "../img"
 javascripts_dir = "../js"
-environment = :development
+fonts_dir = "../fonts"
 relative_assets = true
+environment = :development
 
-# 3. You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
 
-# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
-# output_style = :compressed
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# don't touch this
+# Really don't touch this
 preferred_syntax = :scss
