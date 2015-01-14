1eaeea83863f3140de4a5cdf421f7814c1f49099
joseLuís <senonvero@gmail.com>
2015-01-14 02:08:39 +0100

    improved compass config

--- lib/sass/config.rb
+++ lib/sass/config.rb
@@ -2,14 +2,32 @@
 # This is the compass configuration file
 # Compass is a great cross-platform tool for compiling SASS.
 #
+#    0 Choose Environment (development | production)
+#
+#    1 Require Dependencies
+#
+#    2 Default Paths & Options
+#
+#    3 Environment Options
+#
+#    4 Don't Touch
+#
 # http://compass-style.org/help/documentation/configuration-reference/
 # http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
 
 #
-# 0. Dependencies
+# 0 CHOOSE ENVIRONMENT
 #
-# See style.scss "2 Dependencies"
+
+environment = :development
+#environment = :production		# < uncomment this line on production
+
+
+#
+# 1 REQUIRE DEPENDENCIES
+#
+# See style.scss : "2 Dependencies"
 #
 
 require "compass/import-once/activate"
@@ -21,53 +39,44 @@ require "SassyLists"
 
 
 #
-# 1 Set this to the root of your project when deployed
+# 2 DEFAULT PATHS & OPTIONS
 #
 
 http_path = "/"
-
-
-#
-# 3. You can select your preferred output style here. Use :compressed for production
-#
-
-environment = :development
-#environment = :production		# < enable on production
-
-
-#
-# 3. You can select your preferred output style here. Use :compressed for production
-#
-
-output_style = :expanded
-#output_style = :compressed		# < enable on production
-
-
-#
-# 4. To disable debugging comments that display the original location of your selectors. Uncomment:
-#
-
-#line_comments = false			# < enable on production
-
-
-#
-# 5. Generate source maps. See http://thesassway.com/intermediate/using-source-maps-with-sass
-#
-
-sourcemap = true
-
-
-#
-# 6. Default options and paths. You'll probably don't have to touch these
-#
 
 css_dir = "../css"
 sass_dir = "./"
 images_dir = "../img"
 javascripts_dir = "../js"
 fonts_dir = "../fonts"
+
 relative_assets = true
 
-# don't touch this
+
+#
+# 3 ENVIRONMENT OPTIONS
+#
+
+if environment == :development
+
+	# For Development
+	output_style = :expanded
+	line_comments = true
+	sourcemap = true
+
+else
+
+	# For Production
+	output_style = :compressed
+	line_comments = false
+	sourcemap = false
+
+end
+
+
+#
+# 4 DON'T TOUCH THIS
+#
+
 preferred_syntax = :scss
 
