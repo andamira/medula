1051b89a240e365e4ee4f7719bd48677c8fb3903
joseLuís <senonvero@gmail.com>
2015-01-27 11:34:44 +0100

    Improved Médula intro comments

--- medula/medula.php
+++ medula/medula.php
@@ -7,12 +7,25 @@
  * Author: andamira
  * Author URI: http://andamira.net
  *
- * NOTE: You must customize each of the included files
- * for your particular use case
  *
+ * Médula is a minimal plugin template already included in Ósea.
+ *
+ *   - You can access this functionality very easily, just by uncommenting
+ *     the corresponding line in the functions.php file (section 5).
+ *
+ *   - You could also move this folder into the WordPress plugins folder
+ *     and use it as a standalone plugin, if you prefer to separate
+ *     functionality from presentation.
+ *
+ *   - If you have need for a more complex plugin framework, you could use
+ *     { @link https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate
+ *     Wordpress Plugin Boilerplate} instead,instead, for example.
+ *
+ * NOTE: If you decide to use Médula, you must customize each one 
+ * of the included files for your particular use case
  */
 
-// Add Google Analytics Tracking Code
+// Google Analytics Tracking Code
 include_once( 'analytics.php' );
 
 // Create Shortcodes
