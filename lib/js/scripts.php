30425f945388cce3608fa61dfc4dba3d769ef945
joseLuís <senonvero@gmail.com>
2015-02-07 03:36:38 +0100

    minor updates

--- lib/js/scripts.php
+++ lib/js/scripts.php
@@ -13,8 +13,8 @@ if ( filemtime( 'scripts.js') < filemtime( 'scripts.min.js' ) ) {
 	echo file_get_contents( 'scripts.min.js' );
 } else {
 	$output = \JShrink\Minifier::minify( file_get_contents( 'scripts.js' ) );
-	//$output = file_get_contents( 'scripts.js' );
 	file_put_contents( 'scripts.min.js', $output );
+	# $output = file_get_contents( 'scripts.js' ); // Uncoment to debug
 	echo $output;
 }
 
