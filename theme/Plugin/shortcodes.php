026978865d55f8b6ad3f640de1638d4684a06a98
joseLuís <senonvero@gmail.com>
2015-03-16 01:59:31 +0100

    fix whitespace; new doc link

--- theme/Plugin/shortcodes.php
+++ theme/Plugin/shortcodes.php
@@ -15,25 +15,25 @@
  * Creates a div with any number of additional classes
  *
  * Usage: [medula-shortcode class="example"]
- * 
+ *
  */
 
- function medula_shortcode_example( $atts
-		// Comment the next line for a self-enclosing shortcode
-		, $content = null  
-		) { 
+function medula_shortcode_example( $atts
+	// Comment the next line for a self-enclosing shortcode
+	, $content = null
+	) {
 	
 	// List of attributes with either their default value, or null
-    extract( shortcode_atts(
-        array(
-            'class' => null,
-        ), $atts )
-    );
+	extract( shortcode_atts(
+		array(
+			'class' => null,
+		), $atts )
+	);
 
-    $out = '<div class="medula_shortcode " ' . $class . '">';
-    $out .= $content;
-    $out .= '</div>';
-    return $out;
+	$out = '<div class="medula_shortcode " ' . $class . '">';
+	$out .= $content;
+	$out .= '</div>';
+	return $out;
 }
 add_shortcode( 'medula-shortcode', 'medula_shortcode_example' );
 
