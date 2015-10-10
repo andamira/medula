77dd41cb91782810db851710b7aae3b0c7a5e263
joseLuís <senonvero@gmail.com>
2015-10-10 03:10:53 +0200

    refactorization

--- theme/lib/vendor.php
+++ theme/lib/vendor.php
@@ -2,66 +2,56 @@
 /**
  * EXTERNAL LIBRARIES & PLUGINS TEMPLATES
  *
- *     1 EXT. LIBRARY LOADING
+ *     1 SPECIFIC PLUGINS
+ *
+ *         1.1 WPML
+ *         1.2 TOOLSET
+ *         1.3 WOOCOMMERCE
  *
  *     2 EXT. FIXES
  *
- *     3 EXT. CLEANUP
- *
- *     4 SPECIFIC PLUGINS
- *
- *         4.1 WPML
- *         4.2 TOOLSET
- *         4.3 WOOCOMMERCE
+ *     3 DOWNLOADED LIBRARIES
  */
 
+
 /**
- * 1 Load external libraries
+ * 2 SPECIFIC PLUGINS SUPPORT
  * ************************************************************
  */
-# include_once( '/../vendor/example/example.php' ); // TODO
+
+/**
+ * 1.1 WPML
+ *
+ * @link http://wpml.org/
+ */
+# include_once( 'vendor/wpml.php' );
+
+/**
+ * 1.2 TOOLSET
+ *
+ * @link http://wp-types.org/ Toolset
+ */
+# include_once( 'vendor/toolset.php' );
+
+/**
+ * 1.3 WOOCOMMERCE
+ *
+ * @link http://www.woothemes.com/woocommerce/
+ */
+# include_once( 'vendor/woocommerce.php' );
 
 
 /**
- * 2 Fix problems with external libraries & plugins
+ * 2 Fix misc. problems with some browsers libraries & plugins
  * ************************************************************
  */
 include_once( 'vendor/fixes.php' );
 
 
 /**
- * 3 Cleans & minifies code of external libraries, plugins, etc.
+ * 3 Downloaded Libraries
  * ************************************************************
  */
-# include_once( 'vendor/cleanup.php' );
-
-
-/**
- * 4 SPECIFIC PLUGINS SUPPORT
- * ************************************************************
- */
-
-/**
- * 4.1 WPML
- *
- * @link http://wpml.org/
- */
-# include_once( 'vendor/wpml.php' );         // < disabled by default
-
-/**
- * 4.2 TOOLSET
- *
- * @link http://wp-types.org/ Toolset
- */
-if ( defined( 'TOOLSET_LAYOUTS' ) && TOOLSET_LAYOUTS ) {
-	include_once( 'vendor/toolset.php' );
-}
-
-/**
- * 4.3 WOOCOMMERCE
- *
- * @link http://www.woothemes.com/woocommerce/
- */
-# include_once( 'vendor/woocommerce.php' );  // < disabled by default
+# include_once( '../vendor/example/example.php' ); // TODO
 
 
