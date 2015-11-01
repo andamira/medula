2af0e04816c4cd0bf6975e633f2b57778c2bd639
joseLuís <joseluis@andamira.net>
2015-11-01 20:55:49 +0100

    Fix #36. Remove IE conditional classes in header

--- theme/header-layouts.php
+++ theme/header-layouts.php
@@ -5,12 +5,8 @@
  * @link http://codex.wordpress.org/Function_Reference/get_header
  */
 ?>
-<!doctype html>
-
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->		
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->		
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
 
 <head>
 	<meta charset="utf-8">
