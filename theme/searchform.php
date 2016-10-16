f58b1f9893f2b18c1498da751da35c65b5254a66
joseLuís <joseluis@andamira.net>
2016-10-16 23:27:15 +0200

    change id to class

--- theme/searchform.php
+++ theme/searchform.php
@@ -13,7 +13,7 @@
 
 // <!-- Search Form Type 2: simple text box + icon -->
 /*
-<form id="searchform" role="search" method="get" action="/index.php">
+<form class="searchform" role="search" method="get" action="/index.php">
     <input type="text" name="s" id="s" />
     <button type="submit" title="<?php _e('Search', 'medula')?>">
         <i class="dashicons-search dashicons"></i>
